ad5a65406cf2ee8054841bca4da88c00f5df7d18
Pat Howell <patrick.howell@outlook.com>
2015-02-03 22:09:28 +0000

    Added simple implementation of the guid generator

--- Program.fs
+++ Program.fs
@@ -1,7 +1,19 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System;
+
+let UsageMessage = "This utility takes 2 parameters: \n1) the required length of the Guid \n2) the required number of GUIDs";
+
+let MakeGuid length =
+    Guid.NewGuid().ToString().Replace("-", "").Substring(0, length);
+
+let rec RecurseToCreate possition maximum length createFunction =
+    printfn "%s" (createFunction length)
+    let incrementedPossition = possition + 1;
+    if incrementedPossition <= maximum then 
+        RecurseToCreate incrementedPossition maximum length createFunction
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+    match argv.Length with
+    | 2 -> RecurseToCreate 1 (Int32.Parse(argv.[0])) (Int32.Parse(argv.[1])) MakeGuid
+    | _ -> printfn "%s" UsageMessage
+    0
