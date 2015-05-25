f7488fdba0a93f6ca0f228a74f40180a62641e58
Pat Howell <patrick.howell@outlook.com>
2015-05-25 22:21:42 +0100

    Added basic checking for guid length parameter

--- Program.fs
+++ Program.fs
@@ -4,14 +4,24 @@ open System.Resources;
 open GuidGenerator;
 
 let Assembly = Assembly.GetExecutingAssembly()
-let Resources = new ResourceManager("Resources", Assembly);
+let Resources = new ResourceManager("Resources", Assembly)
 
-let UsageMessage = Resources.GetString("UsageMessage");
+let UsageMessage = Resources.GetString("UsageMessage")
+let InvalidLengthMessage = Resources.GetString("InvalidLengthMessage")
+
+let CheckRequestedGuidLength length = 
+    length > 0 && length <= 36
+
+let MakeGuidsForUser totalRequestedGuids guidLength dashesRequired = 
+    if CheckRequestedGuidLength guidLength then
+        CreateGuids totalRequestedGuids guidLength dashesRequired
+    else
+        printfn "%s" InvalidLengthMessage
 
 [<EntryPoint>]
 let main argv =
     match argv.Length with
-    | 3 -> CreateGuids (Int32.Parse(argv.[1])) (Int32.Parse(argv.[0])) argv.[2]
-    | 2 -> CreateGuids (Int32.Parse(argv.[1])) (Int32.Parse(argv.[0])) "N"
+    | 3 -> MakeGuidsForUser (Int32.Parse(argv.[1])) (Int32.Parse(argv.[0])) argv.[2]
+    | 2 -> MakeGuidsForUser (Int32.Parse(argv.[1])) (Int32.Parse(argv.[0])) "N"
     | _ -> printfn "%s" UsageMessage
     0
